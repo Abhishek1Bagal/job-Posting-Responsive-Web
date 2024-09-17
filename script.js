f924a1943b2426ef5cde9029a2d593d9bb1c7d99
Abhishek1Bagal <110403133+Abhishek1Bagal@users.noreply.github.com>
2024-09-17 21:51:46 +0530

    Add the section
    add the section of which includes
    Heading, paragraph and and the cards.

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ menuBtn.addEventListener("click", () => {
 const isOpen = navLinks.classList.contains("open");
 menuBtnIcon.setAttribute("class" , isOpen? "ri-close-line":"ri-menu-line");
 });
+
+// navLinks.addEventListener("click",(e) =>{
+//     navLinks.classList.remove("open");
+//     menuBtnIcon.setAttribute("class","ri-menu-line");
+// });
+
+const scrollRevealOption = {
+    distance:"50px",
+    origin:"bottom",
+    duration:1000,
+};
+
+ScrollReveal().reveal(".header_container h2",{
+    ...scrollRevealOption,
+   });
+
+ScrollReveal().reveal(".header_container h1",{
+    ...scrollRevealOption,
+    delay:500,
+   });
+
+ScrollReveal().reveal(".header_container p",{
+    ...scrollRevealOption,
+    delay:1000,
+   });
+   ScrollReveal().reveal(".header_btns",{
+    ...scrollRevealOption,
+    delay:1500,
+   });
