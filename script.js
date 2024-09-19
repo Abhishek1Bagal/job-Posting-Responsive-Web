1834fc0a08e138dc95fdbbedc8476159348bfb31
Abhishek1Bagal <110403133+Abhishek1Bagal@users.noreply.github.com>
2024-09-20 01:12:10 +0530

    Add the offer section
    Add the job opening section and what we offer

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
-const menuBtn =document.getElementById("menu-btn");
-const navLinks =document.getElementById("nav-links");
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
 
 const menuBtnIcon = menuBtn.querySelector("i");
 
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 
-const isOpen = navLinks.classList.contains("open");
-menuBtnIcon.setAttribute("class" , isOpen? "ri-close-line":"ri-menu-line");
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
 
-// navLinks.addEventListener("click",(e) =>{
-//     navLinks.classList.remove("open");
-//     menuBtnIcon.setAttribute("class","ri-menu-line");
-// });
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
 
-// const scrollRevealOption = {
-//     distance:"50px",
-//     origin:"bottom",
-//     duration:1000,
-// };
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
 
-// ScrollReveal().reveal(".header_container h2",{
-//     ...scrollRevealOption,
-//    });
+ScrollReveal().reveal(".header_container h2", {
+    ...scrollRevealOption,
+});
 
-// ScrollReveal().reveal(".header_container h1",{
-//     ...scrollRevealOption,
-//     delay:500,
-//    });
+ScrollReveal().reveal(".header_container h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
 
-// ScrollReveal().reveal(".header_container p",{
-//     ...scrollRevealOption,
-//     delay:1000,
-//    });
-//    ScrollReveal().reveal(".header_btns",{
-//     ...scrollRevealOption,
-//     delay:1500,
-//    });
+ScrollReveal().reveal(".header_container p", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".header_btns", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+ScrollReveal().reveal(".steps-card", {
+    ...scrollRevealOption,
+    interval: 500,
+});
+ScrollReveal().reveal(".explore_card", {
+    duration: 1000,
+    interval: 500,
+});
+ScrollReveal().reveal(".job_card", {
+    duration: 1000,
+    interval: 500,
+});
+ScrollReveal().reveal(".offer_card", {
+    ...scrollRevealOption,
+    interval: 500,
+});
